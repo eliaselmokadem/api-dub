981266a8a2c95ae0c7d80677f4a653a1889a6fe8
eliaselmokadem <s141765@ap.be>
2025-02-23 23:24:31 +0100

    del weatherforcast

--- obj/Debug/net8.0/api-personalsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/api-personalsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-personalsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+152ff84faf64b220ac959c11138706afdceb8375")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-personalsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-personalsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
