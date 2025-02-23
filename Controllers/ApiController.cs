152ff84faf64b220ac959c11138706afdceb8375
eliaselmokadem <s141765@ap.be>
2025-02-23 23:22:22 +0100

    first push

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MessageController : ControllerBase
+{
+    // GET api/message
+    [HttpGet]
+    public IActionResult SendMessageByMail()
+    {
+        return Ok("Hello from the GET endpoint!");
+    }
+
+    // POST api/message
+    [HttpPost]
+    public IActionResult PostMessageInDatabase([FromBody] Message message)
+    {
+        // Here you would typically handle the message, e.g., save it to a database
+        return Ok($"Message received with Content: {message.Content}");
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,5 @@
+﻿public class Message
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+}
